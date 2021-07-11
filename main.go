2fd78d5c32dfd87dcbfd7b340f987c8754b2f5de
Roman Hrab <lroman242@gmail.com>
2021-07-11 13:20:50 +0300

    add simple command description

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/lroman242/envdir/utils"
 	"log"
 	"os"
 )
 
 func main()  {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "envdir tool will parse environment variables from directory and run provided" +
+			" command with arguments and parsed environment variables")
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "%s /path/to/directory/ command [options|args...] \n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
 	args := flag.Args()
-	log.Printf("%+v\n", args)
-
+	fmt.Println(args)
 	if len(args) < 2 {
 		log.Fatalf("invalid number of arguments. expect at least 2 arguments")
 	}
@@ -21,9 +29,6 @@ func main()  {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	log.Printf("%+v\n", env)
-	log.Printf("%+v\n", args[1:])
 
 	os.Exit(utils.RunCommand(args[1:], env))
 }
